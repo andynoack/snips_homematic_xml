d96ea1a62d23006917b7431e31784bd34a3b059b
andynoack <andreas.noack@hochschule-stralsund.de>
2019-01-30 22:22:47 +0100

    Add config.ini handling

--- setup.sh
+++ setup.sh
@@ -18,3 +18,15 @@ fi
 . $VENV/bin/activate
 
 pip install -r requirements.txt
+
+if [ -f config.ini ]
+then
+    rm -rf config.ini
+fi
+
+if [ -f /usr/share/snips/assistant/snippets/ndy1982.Homematic/config.ini ]
+then
+    cp /usr/share/snips/assistant/snippets/ndy1982.Homematic/config.ini config.ini
+else
+    cp config.ini.default config.ini
+fi
